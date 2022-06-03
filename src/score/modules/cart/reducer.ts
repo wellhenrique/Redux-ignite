50fb542e5971eb336d1900f06a4c12e525d3c900
wellhenrique <welissonh80@gmail.com>
2022-06-03 10:36:55 -0300

    feat: using immer lib

--- src/score/modules/cart/reducer.ts
+++ src/score/modules/cart/reducer.ts
@@ -1,5 +1,6 @@
 import { Reducer } from "react";
 import { ICartState } from "./types";
+import produce from "immer";
 
 const INITIAL_STATE: ICartState = {
   items: [],
@@ -9,17 +10,21 @@ export const cart: Reducer<ICartState, any> = (
   state = INITIAL_STATE,
   action
 ) => {
-  switch (action.type) {
-    case "ADD_PRODUCT_TO_CART": {
-      const { product } = action.payload;
+  return produce(state, (draft) => {
+    switch (action.type) {
+      case "ADD_PRODUCT_TO_CART": {
+        const { product } = action.payload;
 
-      return {
-        ...state,
-        items: [...state.items, { product, quantity: 1 }],
-      };
+        draft.items.push({
+          product,
+          quantity: 1,
+        });
+
+        break;
+      }
+      default: {
+        return draft;
+      }
     }
-    default: {
-      return state;
-    }
-  }
+  });
 };
