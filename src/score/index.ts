4c41587a44b1f5007e9a63b1fdf3aaaabbc7c612
wellhenrique <welissonh80@gmail.com>
2022-06-03 10:58:03 -0300

    chore: configure ReduxSAGA

--- src/score/index.ts
+++ src/score/index.ts
@@ -1,11 +1,15 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, applyMiddleware } from "@reduxjs/toolkit";
 import { ICartState } from "./modules/cart/types";
 import rootReducer from "./modules/rootReducer";
+import createSagaMiddleware from "@redux-saga/core";
 
 export interface IState {
   cart: ICartState;
 }
 
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: { rootReducer, applyMiddleware(...middlewares); },
 });
